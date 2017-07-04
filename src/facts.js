3d2a477ba4c9ecadf2d58d5174b59a3de17f2771
Jake Lee <jakelee8@icloud.com>
2017-07-04 12:05:00 -0700

    Implement custom fact skill

--- src/facts.js
+++ src/facts.js
@@ -1,12 +1,47 @@
 // TODO Add more or replace entirely the facts below for your chosen history domain
 // Include at least 10 distinct facts with a 4-digit year included in the text for each
+
+// These facts are from WikiPedia:
+// https://en.wikipedia.org/wiki/19th_century_in_film
 module.exports = {
-    "FACTS_EN": [
-        "The field of AI is considered to have its origin in 1950, with publication of British mathematician Alan Turing's paper, Computing Machinery and Intelligence.",
-        "The term, Artificial Intelligence, was coined in 1956 by mathematician and computer scientist John McCarthy, at Dartmouth College, in New Hampshire.",
-        "In 1997, a chess-playing program named Deep Blue, developed by IBM, beat the reigning world chess champion.",
-        "A so-called AI Winter occurred in 1974, when funding was cut after Speech Understanding research did not live up to its promise.",
-        "A driverless robotic car named Stanley, engineered by Sebastian Thrun's Stanford Racing team, sped through the Mojave desert at 22 miles per hour to win the 2005 Darpa Grand Challenge.",
-        "Replace me and add some more facts!"
-    ]
-}
+  'FACTS_EN': [
+    'In 1826, Nicéphore Niépce took the first known photograph in history.',
+    'In 1832, Joseph Plateau (Belgium) and Simon von Stampfer (Vienna) simultaneously invented the Stroboscope. The Stroboscope creates an optical illusion of movement by mounting drawings on the face of a slotted, spinning disk.',
+    'In 1834, the Zoetrope was invented. The Zoetrope was a hollow drum with a strip of pictures around its inner surface. When the drum was spun and the pictures viewed through slots on the side of the drum, the pictures appeared to move.',
+    'In the 1870s, French inventor Charles-Émile Reynaud improved on the Zoetrope with the Praxinoscope by placing mirrors at the center of the drum.',
+    "In 1878, Railroad tycoon Leland Stanford hired British photographer Eadweard Muybridge to photograph a horse in fast motion. Muybridge's experiments inspired French scientist Étienne-Jules Marey to invent equipment for researching animal and human movement.",
+    'In 1879, American George Eastman invented an emulsion-coating machine which enabled the mass production of photographic dry plates.',
+    'In 1880, American George Eastman began to commercially manufacture dry plates for photography.',
+    "In 1880, Eadweard Muybridge held a public demonstration of his Zoopraxiscope, a magic lantern provided with a rotating disc with artist's renderings of Muybridge's chronophotographic sequences.",
+    'On January 1, 1881, American inventor George Eastman founded the Eastman Dry Plate Company.',
+    'In 1882, American inventor George Eastman began experimenting with new types of photographic film with William Walker.',
+    'In 1882, French physiologist Étienne-Jules Marey invented the chronophotographic gun, a camera shaped like a rifle that photographs twelve successive images each second.',
+    'In 1885, American inventors George Eastman and Hannibal Goodwin each invented a sensitized celluloid base roll photographic film to replace the glass plates then in use.',
+    'In 1887, Hannibal Goodwin filed for a patent for his photographic film.',
+    'In 1888, George Eastman filed for a patent for his photographic film.',
+    'In 1888, Thomas Edison met with Eadweard Muybridge to discuss adding sound to moving pictures.',
+    'In 1888, Louis Aimé Augustin Le Prince created the first motion picture films created on paper rolls of film.',
+    "In 1889, American inventor George Eastman's celluloid base roll photographic film became commercially available.",
+    'Around 1890, William K. L. Dickson, working for Thomas Edison, created the first known motion picture films shot in the United States, the Monkeyshines films.',
+    "In 1891, Thomas Edison's employee William K. L. Dickson finished work on a motion-picture camera, called the Kinetograph, and a viewing machine, called the Kinetoscope.",
+    "On May 20, 1891, Thomas Edison held the first public presentation of his Kinetoscope for the National Federation of Women's Clubs.",
+    'On August 24, 1891, Thomas Edison filed for a patent of the Kinetoscope.',
+    'In 1892, in France, Charles-Émile Reynaud began to have public screenings in Paris at the Théâtre Optique, with hundreds of drawings on a reel that he wound through his Zoetrope projector to show 15 minutes of moving images.',
+    'In 1892, The Eastman Company became the Eastman Kodak Company.',
+    'On March 14, 1893, Thomas Edison was granted Patent #493,426 for "An Apparatus for Exhibiting Photographs of Moving Objects", also known as the Kinetoscope.',
+    'In 1893, Thomas Edison built a motion-picture studio near his laboratory. It was dubbed the "Black Maria" by his staff.',
+    'On May 9, 1893, Thomas Edison held the first public exhibition of films shot using his Kinetograph at the Brooklyn Institute. Only one person at a time could use the Kinetoscope.',
+    'On January 7, 1894, Thomas Edison flimed his assistant Fred Ott sneezing with the Kinetoscope at the "Black Maria".',
+    "On April 14, 1894, the first commercial presentation of the Kinetoscope took place in the Holland Brothers' Kinetoscope Parlor at 1155 Broadway, New York City.",
+    'In 1894, Kinetoscope viewing parlors began to open in major cities. Each parlor contained several machines.',
+    'In 1895, in France, brothers Auguste and Louis Lumière invented a hand-held motion picture camera called the Cinématographe. The Lumière brothers created several pivotal short films.',
+    'In November 1895, in Germany, Emil and Max Skladanowsky developed their own film projector.',
+    'In December 1895, in France, Auguste and Louis Lumière held their first public screening of films shot with their Cinématographe.',
+    'In January 1896, in Britain, Birt Acres and Robert W. Paul developed their own film projector, the Theatrograph, later known as the Animatograph.',
+    'In January 1896, in the United States, a projector called the Vitascope was designed by Charles Francis Jenkins and Thomas Armat.',
+    'In April 1896, in New York City, Thomas Edison and Thomas Armat used the Vitascope to project motion pictures for public screenings.',
+    'In 1896, French magician and filmmaker Georges Méliès began experimenting with motion pictures, developing many early special effects techniques, including stop-motion photography.',
+    'In 1897, in Paris, 125 people died during a film screening at the Charity Bazaar, after a curtain cached on fire from the projector lamp.',
+    "In 1896, in France, Pathé-Frères was founded. In the early 1900s, Pathé became the world's largest film equipment and production company, as well as a major producer of phonograph records."
+  ]
+};
--- src/index.js
+++ src/index.js
@@ -1,94 +1,184 @@
 'use strict';
+
 var Alexa = require('alexa-sdk');
-var APP_ID = undefined;  // can be replaced with your app ID if publishing
 var facts = require('./facts');
+
+var APP_ID;  // can be replaced with your app ID if publishing
 var GET_FACT_MSG_EN = [
-    "Here's your fact: "
-]
+  "Here's your fact: ",
+  "Let's see. ",
+  'Hmm. ',
+  'OK. ',
+  ''
+];
+
+var NO_FACT_MSG_EN_YEAR = [
+  "Sorry, I don't have any facts for {}, but",
+  "I don't know that anything happened in film history in {}, but",
+  'Hmm. I have nothing for {}, but '
+];
+
+var NO_FACT_MSG_EN = [
+  "Sorry, I don't have any facts.",
+  'Hmm. I have nothing right now.'
+];
+
+var MISSING_YEAR = [
+  'For what year?',
+  'What year did you want a fact for?',
+  "I'm sorry, for what year did you want a fact?"
+];
+
 // Test hooks - do not remove!
 exports.GetFactMsg = GET_FACT_MSG_EN;
-var APP_ID_TEST = "mochatest";  // used for mocha tests to prevent warning
+var APP_ID_TEST = 'mochatest';  // used for mocha tests to prevent warning
 // end Test hooks
+
 /*
-    TODO (Part 2) add messages needed for the additional intent
-    TODO (Part 3) add reprompt messages as needed
+  TODO (Part 2) add messages needed for the additional intent
+  TODO (Part 3) add reprompt messages as needed
 */
 var languageStrings = {
-    "en": {
-        "translation": {
-            "FACTS": facts.FACTS_EN,
-            "SKILL_NAME": "My History Facts",  // OPTIONAL change this to a more descriptive name
-            "GET_FACT_MESSAGE": GET_FACT_MSG_EN[0],
-            "HELP_MESSAGE": "You can say tell me a fact, or, you can say exit... What can I help you with?",
-            "HELP_REPROMPT": "What can I help you with?",
-            "STOP_MESSAGE": "Goodbye!"
-        }
+  'en': {
+    'translation': {
+      'FACTS': facts.FACTS_EN,
+      'SKILL_NAME': 'Film History Facts',  // OPTIONAL change this to a more descriptive name
+      'GET_FACT_MESSAGE': GET_FACT_MSG_EN,
+      'NO_FACT_MSG_EN': NO_FACT_MSG_EN,
+      'NO_FACT_MSG_EN_YEAR': NO_FACT_MSG_EN_YEAR,
+      'MISSING_YEAR': MISSING_YEAR,
+      'HELP_MESSAGE': 'You can say tell me a fact, or, you can say exit... What can I help you with?',
+      'HELP_REPROMPT': 'What can I help you with?',
+      'STOP_MESSAGE': 'Goodbye!'
     }
-};
-
-exports.handler = function (event, context, callback) {
-    var alexa = Alexa.handler(event, context);
-    alexa.APP_ID = APP_ID;
-    // set a test appId if running the mocha local tests
-    if (event.session.application.applicationId == "mochatest") {
-        alexa.appId = APP_ID_TEST
-    }
-    // To enable string internationalization (i18n) features, set a resources object.
-    alexa.resources = languageStrings;
-    alexa.registerHandlers(handlers);
-    alexa.execute();
+  }
 };
 
 /*
-    TODO (Part 2) add an intent for specifying a fact by year named 'GetNewYearFactIntent'
-    TODO (Part 2) provide a function for the new intent named 'GetYearFact' 
-        that emits a randomized fact that includes the year requested by the user
-        - if such a fact is not available, tell the user this and provide an alternative fact.
-    TODO (Part 3) Keep the session open by providing the fact with :askWithCard instead of :tellWithCard
-        - make sure the user knows that they need to respond
-        - provide a reprompt that lets the user know how they can respond
-    TODO (Part 3) Provide a randomized response for the GET_FACT_MESSAGE
-        - add message to the array GET_FACT_MSG_EN
-        - randomize this starting portion of the response for conversational variety
+  TODO (Part 2) add an intent for specifying a fact by year named 'GetNewYearFactIntent'
+  TODO (Part 2) provide a function for the new intent named 'GetYearFact'
+      that emits a randomized fact that includes the year requested by the user
+      - if such a fact is not available, tell the user this and provide an alternative fact.
+  TODO (Part 3) Keep the session open by providing the fact with :askWithCard instead of :tellWithCard
+      - make sure the user knows that they need to respond
+      - provide a reprompt that lets the user know how they can respond
+  TODO (Part 3) Provide a randomized response for the GET_FACT_MESSAGE
+      - add message to the array GET_FACT_MSG_EN
+      - randomize this starting portion of the response for conversational variety
 */
 
 var handlers = {
-    'LaunchRequest': function () {
-        this.emit('GetFact');
-    },
-    'GetNewFactIntent': function () {
-        this.emit('GetFact');
-    },
-    'GetFact': function () {
-        // Get a random fact from the facts list
-        // Use this.t() to get corresponding language data
-        var factArr = this.t('FACTS');
-        var randomFact = randomPhrase(factArr);
+  'LaunchRequest': function () {
+    this.emit('AMAZON.HelpIntent');
+  },
 
-        // Create speech output
-        var speechOutput = this.t("GET_FACT_MESSAGE") + randomFact;
-        this.emit(':tellWithCard', speechOutput, this.t("SKILL_NAME"), randomFact)
-    },
-    'GetNewYearFactIntent': function () {
-        //TODO your code here
-    },
-    'AMAZON.HelpIntent': function () {
-        var speechOutput = this.t("HELP_MESSAGE");
-        var reprompt = this.t("HELP_MESSAGE");
-        this.emit(':ask', speechOutput, reprompt);
-    },
-    'AMAZON.CancelIntent': function () {
-        this.emit(':tell', this.t("STOP_MESSAGE"));
-    },
-    'AMAZON.StopIntent': function () {
-        this.emit(':tell', this.t("STOP_MESSAGE"));
+  'GetNewFactIntent': function () {
+    this.emit('GetFact');
+  },
+
+  'GetFact': function () {
+    // Get a random fact from the facts list
+    // Use this.t() to get corresponding language data
+    var randomFact = choice(this.t('FACTS'));
+
+    // Create speech output
+    var speechOutput =
+      '<p>' + choice(this.t('GET_FACT_MESSAGE')) + randomFact + '</p><p>' +
+        this.t('HELP_MESSAGE') + '</p>';
+    var reprompt = this.t('HELP_REPROMPT');
+    var skillName = this.t('SKILL_NAME');
+
+    this.emit(':askWithCard', speechOutput, reprompt, skillName, randomFact);
+  },
+
+  'GetNewYearFactIntent': function () {
+    var speechOutput, randomFact, reprompt;
+
+    // Filter facts by year
+    var year = this.event.request.intent.slots.FACT_YEAR.value || '';
+
+    // Prompt for year, though GetNewFactIntent will be matched if no year
+    // is provided.
+    if (!year) {
+      var updatedIntent = this.event.request.intent;
+      reprompt = choice(this.t('MISSING_YEAR'));
+      speechOutput = choice(this.t('MISSING_YEAR'));
+      this.emit(':elicitSlot', 'FACT_YEAR',
+                speechOutput, reprompt, updatedIntent);
+      return;
     }
+
+    var facts = this.t('FACTS');
+    var yearFacts = facts.filter(function (it) {
+      return it.indexOf(year) >= 0;
+    });
+
+    // Get response
+    if (!yearFacts.length) {
+      // No fact for year, provide random fact.
+      randomFact = choice(this.t('FACTS'));
+      speechOutput =
+        choice(this.t('NO_FACT_MSG_EN_YEAR')).replace('{}', year) +
+          ' ' + randomFact;
+    } else {
+      // Get a random fact from the facts list
+      randomFact = choice(yearFacts);
+      speechOutput = choice(this.t('GET_FACT_MESSAGE')) + randomFact;
+    }
+
+    // Create speech output
+    var skillName = this.t('SKILL_NAME');
+    speechOutput = '<p>' + speechOutput + '</p><p>' +
+      this.t('HELP_MESSAGE') + '</p>';
+    reprompt = this.t('HELP_REPROMPT');
+    this.emit(':askWithCard', speechOutput, reprompt, skillName, randomFact);
+  },
+
+  'AMAZON.HelpIntent': function () {
+    var speechOutput = this.t('HELP_MESSAGE');
+    var reprompt = this.t('HELP_MESSAGE');
+    this.emit(':ask', speechOutput, reprompt);
+  },
+
+  'AMAZON.CancelIntent': function () {
+    this.emit(':tell', this.t('STOP_MESSAGE'));
+  },
+
+  'AMAZON.StopIntent': function () {
+    this.emit(':tell', this.t('STOP_MESSAGE'));
+  },
+
+  'SessionEndedRequest': function () {
+    // Use this function to clear up and save any data needed between sessions
+    this.emit('AMAZON.StopIntent');
+  },
+
+  'Unhandled': function () {
+    this.emit('AMAZON.HelpIntent');
+  }
 };
 
-function randomPhrase(phraseArr) {
-    // returns a random phrase
-    // where phraseArr is an array of string phrases
-    var i = 0;
-    i = Math.floor(Math.random() * phraseArr.length);
-    return (phraseArr[i]);
+exports.handler = function (event, context, callback) {
+  var alexa = Alexa.handler(event, context);
+
+  // set a test appId if running the mocha local tests
+  alexa.APP_ID = APP_ID;
+  if (event.session.application.applicationId == 'mochatest') {
+    alexa.appId = APP_ID_TEST;
+  }
+
+  // To enable string internationalization (i18n) features, set a resources object.
+  alexa.resources = languageStrings;
+  alexa.registerHandlers(handlers);
+  alexa.execute();
 };
+
+/**
+ * Choose a random item from the array.
+ *
+ * @param {Array} arr The array
+ * @return {any} The item
+ */
+function choice (arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
