a7cfc6402673dbaed3adc8dc1836e0001fc08c76
Jake Lee <jakelee8@icloud.com>
2017-07-05 23:27:48 -0700

    Clean up messages

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 'use strict';
 
-var Alexa = require('alexa-sdk');
-var facts = require('./facts');
+const Alexa = require('alexa-sdk');
+const facts = require('./facts');
 
-var APP_ID;  // can be replaced with your app ID if publishing
-var GET_FACT_MSG_EN = [
+const APP_ID = null;  // can be replaced with your app ID if publishing
+const GET_FACT_MESSAGE = [
   "Here's your fact: ",
   "Let's see. ",
   'Hmm. ',
@@ -12,43 +12,56 @@ var GET_FACT_MSG_EN = [
   ''
 ];
 
-var NO_FACT_MSG_EN_YEAR = [
+const NO_FACT_YEAR = [
   "Sorry, I don't have any facts for {}, but",
   "I don't know that anything happened in film history in {}, but",
   'Hmm. I have nothing for {}, but '
 ];
 
-var NO_FACT_MSG_EN = [
+const NO_FACT = [
   "Sorry, I don't have any facts.",
   'Hmm. I have nothing right now.'
 ];
 
-var MISSING_YEAR = [
+const MISSING_YEAR = [
   'For what year?',
   'What year did you want a fact for?',
   "I'm sorry, for what year did you want a fact?"
 ];
 
+const HELP_EXAMPLE = [
+  'a fact',
+  'give me a fact',
+  'give me information',
+  'give me something',
+  'tell me a fact',
+  'tell me information',
+  'what do you know about films'
+];
+
 // Test hooks - do not remove!
-exports.GetFactMsg = GET_FACT_MSG_EN;
-var APP_ID_TEST = 'mochatest';  // used for mocha tests to prevent warning
+exports.GetFactMsg = GET_FACT_MESSAGE;
+const APP_ID_TEST = 'mochatest';  // used for mocha tests to prevent warning
 // end Test hooks
 
 /*
   TODO (Part 2) add messages needed for the additional intent
   TODO (Part 3) add reprompt messages as needed
 */
-var languageStrings = {
+
+const languageStrings = {
   'en': {
     'translation': {
       'FACTS': facts.FACTS_EN,
-      'SKILL_NAME': 'Film History Facts',  // OPTIONAL change this to a more descriptive name
-      'GET_FACT_MESSAGE': GET_FACT_MSG_EN,
-      'NO_FACT_MSG_EN': NO_FACT_MSG_EN,
-      'NO_FACT_MSG_EN_YEAR': NO_FACT_MSG_EN_YEAR,
+      'SKILL_NAME': 'Film History Facts',
+      'GET_FACT_MESSAGE': GET_FACT_MESSAGE,
+      'NO_FACT': NO_FACT,
+      'NO_FACT_YEAR': NO_FACT_YEAR,
       'MISSING_YEAR': MISSING_YEAR,
-      'HELP_MESSAGE': 'You can say tell me a fact, or, you can say exit... What can I help you with?',
-      'HELP_REPROMPT': 'What can I help you with?',
+      'HELP_MESSAGE':
+          'You can say tell me a fact, or, you can say exit... What can I help you with?',
+      'HELP_REPROMPT': 'You can ask for a fact by saying: ',
+      'HELP_EXAMPLE': HELP_EXAMPLE,
       'STOP_MESSAGE': 'Goodbye!'
     }
   }
@@ -67,7 +80,7 @@ var languageStrings = {
       - randomize this starting portion of the response for conversational variety
 */
 
-var handlers = {
+const handlers = {
   'LaunchRequest': function () {
     this.emit('AMAZON.HelpIntent');
   },
@@ -77,66 +90,66 @@ var handlers = {
   },
 
   'GetFact': function () {
+    let msg = new Messages(this);
+
     // Get a random fact from the facts list
-    // Use this.t() to get corresponding language data
-    var randomFact = choice(this.t('FACTS'));
+    let fact = msg.fact();
 
     // Create speech output
-    var speechOutput =
-      '<p>' + choice(this.t('GET_FACT_MESSAGE')) + randomFact + '</p><p>' +
-        this.t('HELP_MESSAGE') + '</p>';
-    var reprompt = this.t('HELP_REPROMPT');
-    var skillName = this.t('SKILL_NAME');
+    let factSpeech = msg.getFact(fact);
+    let help = msg.help();
+    let speechOutput = '<p>' + factSpeech + '</p><p>' + help + '</p>';
+    let reprompt = msg.reprompt();
+    let skillName = this.t('SKILL_NAME');
 
-    this.emit(':askWithCard', speechOutput, reprompt, skillName, randomFact);
+    this.emit(':askWithCard', speechOutput, reprompt, skillName, fact);
   },
 
   'GetNewYearFactIntent': function () {
-    var speechOutput, randomFact, reprompt;
-
-    // Filter facts by year
-    var year = this.event.request.intent.slots.FACT_YEAR.value || '';
+    let msg = new Messages(this);
+    let year = this.event.request.intent.slots.FACT_YEAR.value;
 
     // Prompt for year, though GetNewFactIntent will be matched if no year
     // is provided.
     if (!year) {
-      var updatedIntent = this.event.request.intent;
-      reprompt = choice(this.t('MISSING_YEAR'));
-      speechOutput = choice(this.t('MISSING_YEAR'));
-      this.emit(':elicitSlot', 'FACT_YEAR',
-                speechOutput, reprompt, updatedIntent);
+      let speechOutput = msg.missingYear();
+      let reprompt = msg.missingYear();
+      let intent = this.event.request.intent;
+      this.emit(':elicitSlot', 'FACT_YEAR', speechOutput, reprompt, intent);
       return;
     }
 
-    var facts = this.t('FACTS');
-    var yearFacts = facts.filter(function (it) {
+    // Filter facts by year
+    let facts = this.t('FACTS');
+    let yearFacts = facts.filter(function (it) {
       return it.indexOf(year) >= 0;
     });
 
     // Get response
+    let fact, speechOutput;
     if (!yearFacts.length) {
       // No fact for year, provide random fact.
-      randomFact = choice(this.t('FACTS'));
-      speechOutput =
-        choice(this.t('NO_FACT_MSG_EN_YEAR')).replace('{}', year) +
-          ' ' + randomFact;
+      let noFact = msg.noFactYear(year);
+      fact = msg.fact();
+      speechOutput = noFact + ' ' + fact;
     } else {
       // Get a random fact from the facts list
-      randomFact = choice(yearFacts);
-      speechOutput = choice(this.t('GET_FACT_MESSAGE')) + randomFact;
+      fact = choice(yearFacts);
+      speechOutput = msg.getFact(fact);
     }
 
     // Create speech output
-    var skillName = this.t('SKILL_NAME');
-    speechOutput = '<p>' + speechOutput + '</p><p>' +
-      this.t('HELP_MESSAGE') + '</p>';
-    reprompt = this.t('HELP_REPROMPT');
-    this.emit(':askWithCard', speechOutput, reprompt, skillName, randomFact);
+    let help = msg.help();
+    let reprompt = msg.reprompt();
+    let skillName = this.t('SKILL_NAME');
+    speechOutput = '<p>' + speechOutput + '</p><p>' + help + '</p>';
+    this.emit(':askWithCard', speechOutput, reprompt, skillName, fact);
   },
 
   'AMAZON.HelpIntent': function () {
-    var speechOutput = this.t('HELP_MESSAGE');
-    var reprompt = this.t('HELP_MESSAGE');
+    let msg = new Messages(this);
+    let speechOutput = msg.help();
+    let reprompt = msg.help();
     this.emit(':ask', speechOutput, reprompt);
   },
 
@@ -159,7 +172,7 @@ var handlers = {
 };
 
 exports.handler = function (event, context, callback) {
-  var alexa = Alexa.handler(event, context);
+  let alexa = Alexa.handler(event, context);
 
   // set a test appId if running the mocha local tests
   alexa.APP_ID = APP_ID;
@@ -172,6 +185,79 @@ exports.handler = function (event, context, callback) {
   alexa.registerHandlers(handlers);
   alexa.execute();
 };
+
+/**
+ * Helper class for generating messages.
+ */
+class Messages {
+
+  /**
+   * Constructor.
+   *
+   * @param {Object} self Alexa this value in handler context.
+   */
+  constructor(self) {
+    this.t = self.t.bind(self);
+  }
+
+  /**
+   * Get random fact.
+   *
+   * @return {string}
+   */
+  fact () {
+    return choice(this.t('FACTS'));
+  }
+
+  /**
+   * Get random fact with prefix.
+   *
+   * @param {string} [fact] The fact to prefix with an intro or get a random
+   *   fact if undefined.
+   * @return {string}
+   */
+  getFact(fact) {
+    if (!fact) fact = this.fact();
+    return choice(this.t('GET_FACT_MESSAGE')) + fact;
+  }
+
+  /**
+   * Get missing year message.
+   *
+   * @return {string}
+   */
+  missingYear() {
+    return choice(this.t('MISSING_YEAR'));
+  }
+
+  /**
+   * Get no facts for year message.
+   *
+   * @param {number} year
+   * @return {string}
+   */
+  noFactYear(year) {
+    return choice(this.t('NO_FACT_YEAR')).replace('{}', year);
+  }
+
+  /**
+   * Get help message.
+   *
+   * @return {string}
+   */
+  help() {
+    return this.t('HELP_MESSAGE');
+  }
+
+  /**
+   * Get reprompt mesage.
+   *
+   * @return {string}
+   */
+  reprompt() {
+    return this.t('HELP_REPROMPT') + choice(this.t('HELP_EXAMPLE'));
+  }
+}
 
 /**
  * Choose a random item from the array.
